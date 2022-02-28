e691b43791d707836afe75bc52f169ae5e0fcf92
Nicholas Almeida <n.almeida330@gmail.com>
2022-02-27 17:16:06 -0700

    added conditions and funcitons

--- hand.cpp
+++ hand.cpp
@@ -1,6 +1,6 @@
 /*
   YOUR NAME AND COMMENT AND STUFF GO HERE.  PLEASE DELETE THIS LINE.
-  
+
   
   CLASS INVARIANTS:
   - Cards are stored in card_node objects in a linked list.
@@ -22,28 +22,138 @@ using namespace std;
 
 //////////////////////////////////////////////////
 // Constructor 
-
+hand_type::hand_type()
+// create an empty hand
+{
+  head_ptr = NULL;
+  size = 0;
+}
 
 
 
 //////////////////////////////////////////////////
 // Destructor
+hand_type::~hand_type()
+// delete all nodes in the hand
+{
+  // call discard hand
+  discard_hand();
+}
 
 
 
 
 //////////////////////////////////////////////////
 // Getters
+size_t hand_type::size() const
+// return the number of cards in the hand
+{
+  return size;
+}
 
+bool hand_type::has_card(card_type target) const
+// return true if the hand contains the given card
+{
+  card_node *curr_ptr = head_ptr;
+  while (curr_ptr != NULL)
+  {
+    if (curr_ptr->card == target)
+    {
+      return true;
+    }
+    else
+    {
+      curr_ptr = curr_ptr->next;
+    }
+  }
+  return false;
+}
 
+bool hand_type::is_empty() const
+// return true if the hand is empty
+{
+  return (size == 0);
+}
 
+string hand_type::to_string() const
+// return a string displaying the cards in hand separated by spaces
+{
+  string hand_string = "";
+  card_node *curr_ptr = head_ptr;
+  while (curr_ptr != NULL)
+  {
+    hand_string += curr_ptr->card.to_string() + " ";
+    curr_ptr = curr_ptr->next;
+  }
+  return hand_string;
+}
 
 //////////////////////////////////////////////////
 // Modifiers
+void hand_type::add_card(const card_type &newcard)
+// add the card to the hand
+// NEED TO FIX THIS! 
+{
+  card_node *new_node = new card_node(newcard);
+  if (head_ptr == NULL)
+  {
+    head_ptr = new_node;
+  }
+  else
+  {
+    card_node *curr_ptr = head_ptr;
+    while (curr_ptr->next != NULL)
+    {
+      curr_ptr = curr_ptr->next;
+    }
+    curr_ptr->next = new_node;
+  }
+  size+=1;
+}
 
+void hand_type::remove_card(const card_type &target)
+// remove the card if found in the hand
+{
+  card_node *curr_ptr = head_ptr;
+  card_node *prev_ptr = NULL;
+  while (curr_ptr != NULL)
+  {
+    if (curr_ptr->card == target)
+    {
+      if (prev_ptr == NULL)
+      {
+        head_ptr = curr_ptr->next;
+      }
+      else
+      {
+        prev_ptr->next = curr_ptr->next;
+      }
+      delete curr_ptr;
+      size-=1;
+      return;
+    }
+    else
+    {
+      prev_ptr = curr_ptr;
+      curr_ptr = curr_ptr->next;
+    }
+  }
+}
 
-
-
+void hand_type::discard_hand()
+//remove all cards from hand
+{
+  card_node *curr_ptr = head_ptr;
+  card_node *next_ptr;
+  while (curr_ptr != NULL)
+  {
+    next_ptr = curr_ptr->next;
+    delete curr_ptr;
+    curr_ptr = next_ptr;
+  }
+  head_ptr = NULL;
+  size = 0;
+}
 //////////////////////////////////////////////////
 // Private member functions (if any)
 
--- hand.h
+++ hand.h
@@ -19,21 +19,40 @@ public:
   //////////////////////////////////////////////////
   // Constructor & destructor
   hand_type();
+  // Precondition: None.
+  // Postcondition: Creates an empty hand.
   ~hand_type();
+  // Precondition: None.
+  // Postcondition: Deletes all nodes in the hands linked list.
+  //HINT there is a public functionthat might be useful: discard_hand()?
   
   //////////////////////////////////////////////////
   // Getters
   size_t size() const;
+  // Precondition: None.
+  // Postcondition: Returns the number of cards in the hand.
   bool has_card(card_type target) const;
+  // Precondition: A valid card_type object is given.
+  // Postcondition: Returns true if the hand contains the given card or false otherwise.
   bool is_empty() const;
+  // Precondition: None.
+  // Postcondition: Returns true if the hand is empty or false otherwise.
   string to_string() const;
-  
+  // Precondition: None.
+  // Postcondition: Returns a string displaying the cards in hand separated by spaces.
+  // Example: 9C QS 4D 10D
   
   //////////////////////////////////////////////////
   // Modifiers
   void add_card(const card_type &newcard);
+  // Precondition: A valid card_type object is given.
+  // Postcondition: Adds the given card to the hand.
   void remove_card(const card_type &target);
+  // Precondition: A valid card_type object is given.
+  // Postcondition: Removes the given card from the hand if found.
   void discard_hand();
+  // Precondition: None.
+  // Postcondition: Removes all cards from the hand.
   
   
 private:
